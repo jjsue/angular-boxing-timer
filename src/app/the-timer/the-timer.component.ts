7c9a01f60183ba4287144ca36227a9e3b0b785ef
jjsue <jjsue_95@hotmail.es>
2021-11-22 14:08:17 +0100

    Falta practicamente toda la funcionalidad pero realmente lo que queria probar era la comunicacion entre componentes hermanos y eso esta realizado.

--- src/app/the-timer/the-timer.component.ts
+++ src/app/the-timer/the-timer.component.ts
@@ -7,33 +7,42 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TheTimerComponent implements OnInit {
 
-  @Input() timeDataChild:any;
+  @Input() timeDataChild: any;
 
-  timerMinutes:number = 0;
-  timerSeconds:number = 0;
-  timerRest:number = 0;
+  timerMinutes: number = 0;
+  timerSeconds: number = 0;
+  timerRest: number = 0;
   totalRounds: number = 0;
   currentRound: number = 1;
 
-  btnText:string="Start";
+  manageInterval: any;
 
-  btnClick(){
-    console.log("btnClick");
+  btnText: string = "Start";
+
+  btnClick() {
+    this.timingFunction();
+  }
+
+  timingFunction() {
+    setTimeout(() => {
+      alert("Terminado");
+    }, ((this.timerMinutes * 60) + this.timerSeconds) * 1000);
   }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.timerMinutes= this.timeDataChild[0];
-    this.timerSeconds= this.timeDataChild[1];
-    this.timerRest= this.timeDataChild[2];
-    this.totalRounds= this.timeDataChild[3];
+    this.timerMinutes = this.timeDataChild[0];
+    this.timerSeconds = this.timeDataChild[1];
+    this.timerRest = this.timeDataChild[2];
+    this.totalRounds = this.timeDataChild[3];
   }
-  ngOnChanges(){
-    this.timerMinutes= this.timeDataChild[0];
-    this.timerSeconds= this.timeDataChild[1];
-    this.timerRest= this.timeDataChild[2];
-    this.totalRounds= this.timeDataChild[3];
+  ngOnChanges() {
+    console.log("Changes");
+    this.timerMinutes = this.timeDataChild[0];
+    this.timerSeconds = this.timeDataChild[1];
+    this.timerRest = this.timeDataChild[2];
+    this.totalRounds = this.timeDataChild[3];
   }
 
 }
